27f6b2be281b59401a9cbc6b45b8f8a0364206a0
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-04-07 14:50:41 +0330

    (refactor): Refactor the code

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -3,6 +3,7 @@ import { CartContext } from "src/providers/cart-provider";
 
 export const useCart = () => {
   const context = useContext(CartContext);
+  
   if (!context) throw new Error("useCart must be used within a CartProvider");
 
   return context;
